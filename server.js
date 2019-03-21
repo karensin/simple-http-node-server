7cda35a0ee02b52e5c3ba8bc20c86e198168965e
Karen k  sin <karenksin@Karens-MacBook-Air.local>
2019-03-20 22:20:12 -0700

    node server

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+
+
+const requestHandler = (request, response) => {
+	fs.writeFile('hello-world.txt', 'Hello to this great world!', 'utf8', (err) => {
+	    if (err) throw err;
+	    console.log('success')
+	 });
+	response.end(`Handling a request on port ${port}`)
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+	if (err) {
+    	return console.log(`You have an error:  ${err}`);
+  	}
+  	console.log(`server is listening on ${port}`);
+});
